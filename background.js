1b7871c6ae7b7c952f2597b0c12e3253a9833315
Andrew Sun <andrew@crossbowffs.com>
2024-04-02 23:13:33 -0700

    Use auth header instead of redirect, add DDG

--- background.js
+++ background.js
@@ -2,26 +2,87 @@ if (typeof browser === "undefined") {
   globalThis.browser = chrome;
 }
 
-async function reload() {
-  const storageKey = "session-link";
-  const sessionLink = (await browser.storage.local.get(storageKey))[storageKey];
-  const pattern = sessionLink ? sessionLink + "&q=\\2" : "https://kagi.com/search?q=\\2";
-  const oldRules = await browser.declarativeNetRequest.getDynamicRules();
-  await browser.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: oldRules.map(rule => rule.id),
-    addRules: [{
-      id: 1,
+async function getSessionToken() {
+  const key = "session-link";
+  const link = (await browser.storage.local.get(key))[key];
+  if (!link) {
+    return null;
+  }
+  const pattern = /https:\/\/kagi.com\/search\?token=([A-Za-z0-9._]+)/;
+  const match = link.match(pattern);
+  if (!match) {
+    return null;
+  }
+  return match[1];
+}
+
+function getRedirectRules() {
+  return [
+    {
       condition: {
-        regexFilter: "^https://www.google.com/search\\?(q=|.*&q=)([^&]+).*",
+        urlFilter: "||google.com/search?",
         resourceTypes: ["main_frame"],
       },
       action: {
         type: "redirect",
         redirect: {
-          regexSubstitution: pattern,
+          transform: {
+            host: "kagi.com",
+          },
         },
       },
-    }],
+    },
+    {
+      condition: {
+        urlFilter: "||duckduckgo.com/",
+        resourceTypes: ["main_frame"],
+      },
+      action: {
+        type: "redirect",
+        redirect: {
+          transform: {
+            host: "kagi.com",
+            path: "/search",
+          },
+        },
+      },
+    },
+  ];
+}
+
+function getAuthRule(sessionToken) {
+  if (!sessionToken) {
+    return null;
+  }
+
+  return {
+    condition: {
+      requestDomains: ["kagi.com"],
+      resourceTypes: ["main_frame"],
+    },
+    action: {
+      type: "modifyHeaders",
+      requestHeaders: [{
+        header: "Authorization",
+        operation: "set",
+        value: sessionToken,
+      }],
+    },
+  };
+}
+
+async function reload() {
+  const sessionToken = await getSessionToken();
+  const oldRules = await browser.declarativeNetRequest.getDynamicRules();
+  const rules = getRedirectRules();
+  if (sessionToken) {
+    rules.push(getAuthRule(sessionToken));
+  }
+  rules.forEach((rule, index) => rule.id = index + 1);
+
+  await browser.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: oldRules.map(rule => rule.id),
+    addRules: rules,
   });
 }
 
