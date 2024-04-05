e6a69acf959c400909426985017e21c5541f0c2b
Andrew Sun <andrew@crossbowffs.com>
2024-04-04 23:02:57 -0700

    Nevermind, Safari disappoints once again

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ function getSearchQueryPatterns(base, q, t) {
   // No way to distinguish searches from address bar vs. !bang, always redirect
   if (!t) {
     // google.com/search?q=foo
-    return [`^${base}\\?(?:.*&)?${q}=([^&]+).*`];
+    return [new RegExp(`^${base}\\?(?:.*&)?${q}=([^&]+).*`)];
   }
 
   // Only redirect if the search was initiated via address bar to avoid !bang loops
@@ -14,29 +14,37 @@ function getSearchQueryPatterns(base, q, t) {
   // https://github.com/WebKit/WebKit/blob/0d834ddfd6bb278d8074b0757594dc6698614ad5/Source/WebKit/UIProcess/API/APIContentRuleList.cpp#L63-L75
   return [
     // google.com/search?q=foo&client=bar
-    `^${base}\\?(?:.*&)?${q}=([^&]+).*&${t}=.*`,
+    new RegExp(`^${base}\\?(?:.*&)?${q}=([^&]+).*&${t}=.*`),
     // google.com/search?client=bar&q=foo
-    `^${base}\\?(?:.*&)?${t}=.*&${q}=([^&]+).*`,
+    new RegExp(`^${base}\\?(?:.*&)?${t}=.*&${q}=([^&]+).*`),
   ];
 }
 
-function getRedirectRules() {
-  const patterns = [
-    getSearchQueryPatterns("https://www.google.com/search", "q", "client"),
-    getSearchQueryPatterns("https://duckduckgo.com/", "q", "t"),
-  ].flat();
+const searchQueryPatterns = [
+  getSearchQueryPatterns("https://www.google.com/search", "q", "client"),
+  getSearchQueryPatterns("https://duckduckgo.com/", "q", "t"),
+].flat();
 
-  return patterns.map((pattern) => ({
+const redirectBaseUrl = "https://kagi.com/search?q=";
+
+function getRedirectRules() {
+  // DNR redirect doesn't work by itself because WebKit is horribly broken and behaves as if the
+  // destination is loaded in the context of the source page (meaning CSP, cookie domains, etc. are
+  // applied based on the source page). Therefore, we combine it with onBeforeNavigate(), which
+  // works but is asynchronous and so leaks the search request to the source page. The DNR redirect
+  // serves to ensure that the original search request doesn't go through while we wait for
+  // onBeforeNavigate() to kick in. Note that for some reason, blackholing the request to 0.0.0.0
+  // in the DNR redirect doesn't work. For more info see:
+  // https://github.com/kagisearch/browser_extensions/pull/59#issuecomment-1876520441
+  return searchQueryPatterns.map((pattern) => ({
     condition: {
-      regexFilter: pattern,
+      regexFilter: pattern.source,
       resourceTypes: ["main_frame"],
     },
     action: {
       type: "redirect",
       redirect: {
-        // Use www.kagi.com instead of kagi.com to work around Safari bug
-        // See https://github.com/kagisearch/browser_extensions/pull/59#issuecomment-1876520441
-        regexSubstitution: "https://www.kagi.com/search?q=\\1",
+        regexSubstitution: redirectBaseUrl + "\\1",
       },
     },
   }));
@@ -91,5 +99,17 @@ async function reloadWebRequestRules() {
   });
 }
 
+async function onBeforeNavigate(details) {
+  for (const pattern of searchQueryPatterns) {
+    const match = details.url.match(pattern);
+    if (match) {
+      const url = redirectBaseUrl + match[1];
+      await browser.tabs.update(details.tabId, {url});
+      break;
+    }
+  }
+}
+
 browser.runtime.onInstalled.addListener(reloadWebRequestRules);
 browser.storage.local.onChanged.addListener(reloadWebRequestRules);
+browser.webNavigation.onBeforeNavigate.addListener(onBeforeNavigate);
